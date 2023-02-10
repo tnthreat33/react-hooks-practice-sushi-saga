2b41184c3bf1832c875f7e589b8fcb530c326260
Rachel Hamby <tnthreat33@gmail.com>
2023-02-10 18:34:23 -0500

    commit tree a9b9563b4567acb3d13d63a5eb8034a97bf12527

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ sushis, onEatSushi }) {
+  const [sushiIndex, setSushiIndex] = useState(0);
+
+  const sushiComponents = sushis
+    .slice(sushiIndex, sushiIndex + 4)
+    .map((sushi) => (
+      <Sushi key={sushi.id} sushi={sushi} onEatSushi={onEatSushi} />
+    ));
+
+  function handleClickMore() {
+    // (sushiIndex + 4) % sushis.length
+    // is a way to wrap back around to the beginning of the array once we get to the end
+    // using the remainder (%) operator
+    setSushiIndex((sushiIndex) => (sushiIndex + 4) % sushis.length);
+  }
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiComponents}
+      <MoreButton onClickMore={handleClickMore} />
     </div>
   );
 }
 
 export default SushiContainer;
+
